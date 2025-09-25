e9c8db58aba63d5ec8373526a5fb472a0935489a
taanush9905 <taanush9905@gmail.com>
2025-09-25 16:17:57 +0530

    Update dsa15.cpp

--- dsa15.cpp
+++ dsa15.cpp
@@ -1,23 +1,47 @@
-//Write a program to delete an element from an array at a given position
+//Implement a sparse matrix using a linked list.
 #include <iostream>
 using namespace std;
 
-int main() {
-    int arr[100], n, pos;
-    cout << "Enter size of array: ";
-    cin >> n;
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    cout << "Enter position to delete (0-based index): ";
-    cin >> pos;
-    for (int i = pos; i < n-1; i++)
-        arr[i] = arr[i+1];
-    n--;
-    cout << "Updated array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-    return 0;
+struct Node {
+    int row, col, val;
+    Node* next;
+};
+
+Node* head = NULL;
+
+void insert(int r, int c, int v) {
+    Node* temp = new Node{r, c, v, NULL};
+    if (!head) {
+        head = temp;
+        return;
+    }
+    Node* p = head;
+    while (p->next)
+        p = p->next;
+    p->next = temp;
 }
 
+void display() {
+    cout << "Row Col Val\n";
+    Node* p = head;
+    while (p) {
+        cout << p->row << " " << p->col << " " << p->val << "\n";
+        p = p->next;
+    }
+}
+
+int main() {
+    int m, n;
+    cout << "Enter rows and cols: ";
+    cin >> m >> n;
+    int mat[20][20];
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+            if (mat[i][j] != 0)
+                insert(i, j, mat[i][j]);
+        }
+    display();
+    return 0;
+}
