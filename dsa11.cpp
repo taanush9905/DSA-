c91e93a657aa71724824513f017b998cd344a4a8
taanush9905 <taanush9905@gmail.com>
2025-09-25 16:13:05 +0530

    Update dsa11.cpp

--- dsa11.cpp
+++ dsa11.cpp
@@ -1,20 +1,58 @@
-//Write a program to copy all elements of one array into another array
+//write a program to remove duplicates from a singly linked list and count the number of duplicates.
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
-using namespace std;
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+int countDuplicates(struct Node* head) {
+    int count = 0;
+    struct Node* ptr1 = head;
+    while (ptr1 != NULL) {
+        struct Node* ptr2 = ptr1->next;
+        while (ptr2 != NULL) {
+            if (ptr1->data == ptr2->data) {
+                count++;
+                break;
+            }
+            ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+    return count;
+}
 
 int main() {
-    int arr1[100], arr2[100], n;
-    cout << "Enter size of array: ";
-    cin >> n;
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr1[i];
-    for (int i = 0; i < n; i++)
-        arr2[i] = arr1[i];
-    cout << "Copied array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr2[i] << " ";
-    cout << endl;
+    struct Node* head = NULL;
+    int n, value;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter value for node %d: ", i+1);
+        scanf("%d", &value);
+        insertEnd(&head, value);
+    }
+    printf("Number of duplicate elements: %d\n", countDuplicates(head));
     return 0;
 }
